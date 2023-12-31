5f558af1d05005122db9d3b61cce072768cb3cfe
spopa <spopa@taliesinllc.com>
2023-12-31 16:49:59 +0200

    testing package publishing.

---
+++ ports/nanojsonc/portfile.cmake
@@ -0,0 +1,35 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+OUT_SOURCE_PATH SOURCE_PATH
+REPO saadshams/nanojsonc
+REF "${VERSION}"
+SHA512 22d8e1b97260dfeaaf47f8a4d9bbf8bb74e955f1b22b27a4cd5a3a115fcffceba17993949ac1a0522be0213c39ed34c0f44e58f02536196473981eeeab0c5eac
+HEAD_REF main
+)
+
+
+
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS -DBUILD_TESTS=OFF)
+
+
+
+vcpkg_cmake_install()
+
+
+
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/nanojsoncTargets")
+
+
+
+#file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+
+
+# Install License
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+
+
+# Install Usage
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
